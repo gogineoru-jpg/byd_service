52b066fe484876603c8a29ebe8e9300856392bad
gogineoru-jpg <gogi.neo.ru@gmail.com>
2026-09-18 20:04:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import secrets
 from datetime import datetime, date
 from fastapi import FastAPI, Request, Form, Depends, HTTPException, status
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey, DateTime, or_, text, func
 from sqlalchemy.orm import declarative_base, sessionmaker, Session, relationship
